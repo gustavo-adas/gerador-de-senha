d498e0a9809fcab1f7b3fac7e56a2639fbcfdfef
Gustavo L Adas <gustavo_adas@hotmail.com>
2022-06-08 00:00:04 -0300

    confirmar master

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let sliderElement = document.querySelector("#slider");
 let buttonElement = document.querySelector("#button");
 
 let sizePassword = document.querySelector("#valor");
-let password = document.querySelector("password");
+let password = document.querySelector("#password");
 
 let containerPassword = document.querySelector("#container-password");
 
